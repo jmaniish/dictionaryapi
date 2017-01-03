43dee22b7a2870dc9c4a5b9e8a5e3159c32bf95f
maniish-jaiin <jaiin.maniish@gmail.com>
2017-01-03 16:57:54 +0530

    added viewModel and databinding.

--- app/src/main/java/experiment/textfield/com/dictionaryapi/views/MainActivity.java
+++ app/src/main/java/experiment/textfield/com/dictionaryapi/views/MainActivity.java
@@ -1,56 +1,22 @@
 package experiment.textfield.com.dictionaryapi.views;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import experiment.textfield.com.dictionaryapi.R;
-import experiment.textfield.com.dictionaryapi.models.DictionaryResponse;
+import experiment.textfield.com.dictionaryapi.databinding.ActivityMainBinding;
 import experiment.textfield.com.dictionaryapi.network.RestFactory;
-import rx.Observer;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView resultView;
-    private RestFactory restFactory;
+    private MainActivityViewModel mainActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button goButton = (Button) findViewById(R.id.go_btn);
-        final EditText wordEditText = (EditText) findViewById(R.id.word_entry);
-        resultView = (TextView) findViewById(R.id.result);
-
-        restFactory = new RestFactory();
-        goButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                restFactory.getDefinition().getDefinition(wordEditText.getText().toString()).subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<DictionaryResponse>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(DictionaryResponse dictionaryResponse) {
-                        resultView.setText(dictionaryResponse.results[0].getLexicalEntries()[0]
-                                .getEntries()[0].getSenses()[0].getDefinitions()[0]);
-                    }
-                });
-            }
-        });
+        mainActivityViewModel = new MainActivityViewModel(new RestFactory());
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainBinding.setMainViewModel(mainActivityViewModel);
     }
-
 }
--- app/src/main/java/experiment/textfield/com/dictionaryapi/views/MainActivityViewModel.java
+++ app/src/main/java/experiment/textfield/com/dictionaryapi/views/MainActivityViewModel.java
@@ -1,4 +1,43 @@
 package experiment.textfield.com.dictionaryapi.views;
 
+import android.databinding.ObservableField;
+import android.view.View;
+
+import experiment.textfield.com.dictionaryapi.models.DictionaryResponse;
+import experiment.textfield.com.dictionaryapi.network.RestFactory;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class MainActivityViewModel {
+
+    public ObservableField<String> wordToSearch = new ObservableField<>("");
+    private RestFactory restFactory;
+    public ObservableField<String> wordDefinition = new ObservableField<>("");
+
+    public MainActivityViewModel(RestFactory restFactory) {
+        this.restFactory = restFactory;
+    }
+
+    public void findDefinitionForWord(View view) {
+        restFactory.getDefinition().getDefinition(wordToSearch.get()).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<DictionaryResponse>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(DictionaryResponse dictionaryResponse) {
+                wordDefinition.set(dictionaryResponse.results[0].getLexicalEntries()[0]
+                        .getEntries()[0].getSenses()[0].getDefinitions()[0]);
+            }
+        });
+    }
+
 }
